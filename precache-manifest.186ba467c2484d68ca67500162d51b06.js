8f0b53b8c50c0fb32f9a8e740fc403cf2812e3d2
NaFaPaz <nfanego@gmail.com>
2022-05-18 12:05:23 -0300

    Updates

---
+++ precache-manifest.186ba467c2484d68ca67500162d51b06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59fd851dcc181c0422068b78c83b40eb",
+    "url": "/KimcheDevChallenge/index.html"
+  },
+  {
+    "revision": "828cb3523b155184a2fb",
+    "url": "/KimcheDevChallenge/static/css/main.8c8b4e8a.chunk.css"
+  },
+  {
+    "revision": "12a5013794c98e0487e9",
+    "url": "/KimcheDevChallenge/static/js/2.5cda998d.chunk.js"
+  },
+  {
+    "revision": "49eb8843f2ec5480c31ee4f7ce295c84",
+    "url": "/KimcheDevChallenge/static/js/2.5cda998d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "828cb3523b155184a2fb",
+    "url": "/KimcheDevChallenge/static/js/main.13830b07.chunk.js"
+  },
+  {
+    "revision": "41eb33039cda0743c49d",
+    "url": "/KimcheDevChallenge/static/js/runtime-main.c65bf391.js"
+  }
+]);
